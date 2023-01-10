5056560c466b3608166102c330ed0f3c7e626153
gestureleft <harry130899@hotmail.com>
2023-01-10 15:06:14 +1100

    Remove some calls to deprecetated functions
    Also setup eslint

--- lua/lsp-config/language-servers.lua
+++ lua/lsp-config/language-servers.lua
@@ -9,7 +9,7 @@ vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-	client.resolved_capabilities.document_formatting = false
+	-- client.server_capabilities.document_formatting = false
 	-- Enable completion triggered by <c-x><c-o>
 	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
@@ -30,7 +30,7 @@ local on_attach = function(client, bufnr)
 	vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
 	vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
 	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-	vim.keymap.set("n", "<space>f", vim.lsp.buf.formatting, bufopts)
+	vim.keymap.set("n", "<space>f", vim.lsp.buf.format, bufopts)
 end
 
 -- Add additional capabilities supported by nvim-cmp
@@ -78,3 +78,5 @@ require("lspconfig")["clangd"].setup({
 		"--clang-tidy",
 	},
 })
+require("lspconfig").eslint.setup({})
+require("lspconfig")["vls"].setup({})
